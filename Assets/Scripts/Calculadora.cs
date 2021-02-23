d4b1b9b135306fe01c46768c89f8104966b74993
Daniel <cdmx1963@amerike.edu.mx>
2021-02-23 13:55:55 -0600

    Puse todo en el repo :3... yo solito sin ayuda

---
+++ Assets/Scripts/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculadora : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    public float Magnitud;
+    public GameObject Sphere;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Sphere.transform.position.x;
+        y = Sphere.transform.position.y;
+        z = Sphere.transform.position.z;
+        Magnitud = Mathf.Sqrt((x * x) + (y * y) + (z * z));
+        Debug.Log("El Resultado de la Magnitud es" +  Magnitud);
+
+        //double res = (Mathf.Sqrt((x * x) + (y * y) + (z * z)));
+        //Debug.Log(res);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
