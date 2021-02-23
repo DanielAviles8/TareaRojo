b6d3ece7c32d587cbba7f5450a9fa2bab02d9b89
Daniel <cdmx1963@amerike.edu.mx>
2021-02-23 14:23:28 -0600

    Ya quedo todoski; Magnitud, Normalizar y Distancia con dos objetos

--- Assets/Scripts/Calculadora.cs
+++ Assets/Scripts/Calculadora.cs
@@ -9,16 +9,22 @@ public class Calculadora : MonoBehaviour
     public float z;
     public float Magnitud;
     public GameObject Sphere;
-
+    public float NormalizarX;
+    public float NormalizarY;
+    public float NormalizarZ;
+    public float x1;
+    public float y1;
+    public float z1;
+    public GameObject Cube;
+    public float distanciaX;
+    public float distanciaY;
+    public float distanciaZ;
+    public float DistanciaTotal;
 
     // Start is called before the first frame update
     void Start()
     {
-        x = Sphere.transform.position.x;
-        y = Sphere.transform.position.y;
-        z = Sphere.transform.position.z;
-        Magnitud = Mathf.Sqrt((x * x) + (y * y) + (z * z));
-        Debug.Log("El Resultado de la Magnitud es" +  Magnitud);
+        
 
         //double res = (Mathf.Sqrt((x * x) + (y * y) + (z * z)));
         //Debug.Log(res);
@@ -27,6 +33,27 @@ public class Calculadora : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = Sphere.transform.position.x;
+        y = Sphere.transform.position.y;
+        z = Sphere.transform.position.z;
+        Magnitud = Mathf.Sqrt((x * x) + (y * y) + (z * z));
+        Debug.Log("El Resultado de la Magnitud es" + Magnitud);
+
+        NormalizarX = x / Magnitud;
+        NormalizarY = y / Magnitud;
+        NormalizarZ = z / Magnitud;
+        //Ya solo me falta distancia :c
+        //x1-x2, resultado al cuadrado. Con yz tambien. Sumar 3 resultados
+
+        x1 = Cube.transform.position.x;
+        y1 = Cube.transform.position.y;
+        z1 = Cube.transform.position.z;
+
+        distanciaX = x - x1;
+        distanciaY = y - y1;
+        distanciaZ = z - z1;
+
+        DistanciaTotal = (distanciaX * distanciaX) + (distanciaY * distanciaY) + (distanciaZ * distanciaZ);
+        Debug.Log(DistanciaTotal);
     }
 }
