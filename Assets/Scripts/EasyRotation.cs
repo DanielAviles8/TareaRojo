31ad115801fc0de2c95908e6dddcbd7013de8dc6
Daniel <cdmx1963@amerike.edu.mx>
2021-05-12 18:00:47 -0500

    :3

---
+++ Assets/Scripts/EasyRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyRotation : MonoBehaviour
+{
+    MeshFilter filter;
+	[SerializeField] private Vector3 dir;
+    // Start is called before the first frame update
+    void Start()
+	{
+		Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    gameObject.transform.Rotate(dir);
+	    
+    }
+}
